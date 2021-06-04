2e5a840b281cff84341cca0c213e165f7e10965e
Leon <Liangz.org@gmail.com>
2021-06-04 23:55:59 +0800

    docs: complete hash table part 03

--- C/Write.a.Hash.Table/src/hash_table.c
+++ C/Write.a.Hash.Table/src/hash_table.c
@@ -52,3 +52,13 @@ void ht_del_hash_table(ht_hash_table* ht) {
     free(ht);
 }
 
+// hash_table.c
+static int ht_hash(const char* s, const int a, const int m) {
+    long hash = 0;
+    const int len_s = strlen(s);
+    for (int i = 0; i < len_s; i++) {
+        hash += (long)pow(a, len_s - (i+1)) * s[i];
+        hash = hash % m;
+    }
+    return (int)hash;
+}
--- C/Write.a.Hash.Table/src/hash_table.h
+++ C/Write.a.Hash.Table/src/hash_table.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 typedef struct {
     char* key;
