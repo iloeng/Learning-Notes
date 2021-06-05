f168c684ed330b56b1ac8d3f250832e593562015
Leon <Liangz.org@gmail.com>
2021-06-06 00:34:24 +0800

    docs: complete hash table part 05

--- C/Write.a.Hash.Table/src/hash_table.c
+++ C/Write.a.Hash.Table/src/hash_table.c
@@ -16,7 +16,11 @@
 
 #include "hash_table.h"
 
-static ht_item* ht_new_item(const char* k, const char* v) {
+// hash_table.c
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
+
+static ht_item* ht_new_item(const char* k, const char* v)
+{
     ht_item* i = malloc(sizeof(ht_item));
     i->key = strdup(k);
     i->value = strdup(v);
@@ -24,7 +28,8 @@ static ht_item* ht_new_item(const char* k, const char* v) {
 }
 
 // hash_table.c
-ht_hash_table* ht_new() {
+ht_hash_table* ht_new()
+{
     ht_hash_table* ht = malloc(sizeof(ht_hash_table));
 
     ht->size = 53;
@@ -34,14 +39,16 @@ ht_hash_table* ht_new() {
 }
 
 // hash_table.c
-static void ht_del_item(ht_item* i) {
+static void ht_del_item(ht_item* i)
+{
     free(i->key);
     free(i->value);
     free(i);
 }
 
 
-void ht_del_hash_table(ht_hash_table* ht) {
+void ht_del_hash_table(ht_hash_table* ht)
+{
     for (int i = 0; i < ht->size; i++) {
         ht_item* item = ht->items[i];
         if (item != NULL) {
@@ -53,10 +60,12 @@ void ht_del_hash_table(ht_hash_table* ht) {
 }
 
 // hash_table.c
-static int ht_hash(const char* s, const int a, const int m) {
+static int ht_hash(const char* s, const int a, const int m)
+{
     long hash = 0;
     const int len_s = strlen(s);
-    for (int i = 0; i < len_s; i++) {
+    for (int i = 0; i < len_s; i++)
+    {
         hash += (long)pow(a, len_s - (i+1)) * s[i];
         hash = hash % m;
     }
@@ -64,37 +73,49 @@ static int ht_hash(const char* s, const int a, const int m) {
 }
 
 // hash_table.c
-static int ht_get_hash(
-        const char* s, const int num_buckets, const int attempt
-) {
+static int ht_get_hash(const char* s, const int num_buckets, const int attempt)
+{
     const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
     const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
     return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
 
 // hash_table.c
-void ht_insert(ht_hash_table* ht, const char* key, const char* value) {
+void ht_insert(ht_hash_table* ht, const char* key, const char* value)
+{
     ht_item* item = ht_new_item(key, value);
     int index = ht_get_hash(item->key, ht->size, 0);
     ht_item* cur_item = ht->items[index];
     int i = 1;
-    while (cur_item != NULL) {
-        index = ht_get_hash(item->key, ht->size, i);
-        cur_item = ht->items[index];
-        i++;
+    while (cur_item != NULL)
+    {
+        if (cur_item != &HT_DELETE_ITEM)
+        {
+            if (strcmp(cur_item->key, key) == 0)
+            {
+                ht_del_item(cur_item);
+                ht->items[index] = item;
+                return;
+            }
+        }
     }
     ht->items[index] = item;
     ht->count++;
 }
 
 // hash_table.c
-char* ht_search(ht_hash_table* ht, const char* key) {
+char* ht_search(ht_hash_table* ht, const char* key)
+{
     int index = ht_get_hash(key, ht->size, 0);
     ht_item* item = ht->items[index];
     int i = 1;
-    while (item != NULL) {
+    while (item != NULL)
+    {
         if (strcmp(item->key, key) == 0) {
-            return item->value;
+            if (strcmp(item->key, key) == 0)
+            {
+                return item->value;
+            }
         }
         index = ht_get_hash(key, ht->size, i);
         item = ht->items[index];
@@ -102,10 +123,6 @@ char* ht_search(ht_hash_table* ht, const char* key) {
     }
     return NULL;
 }
-
-// hash_table.c
-static ht_item HT_DELETED_ITEM = {NULL, NULL};
-
 
 void ht_delete(ht_hash_table* ht, const char* key) {
     int index = ht_get_hash(key, ht->size, 0);
