da52fb2fe3d095854573b7b041334f5924e4766b
Leon <Liangz.org@gmail.com>
2021-06-05 23:05:43 +0800

    docs: complete hash table part 04

--- C/Write.a.Hash.Table/src/hash_table.c
+++ C/Write.a.Hash.Table/src/hash_table.c
@@ -62,3 +62,13 @@ static int ht_hash(const char* s, const int a, const int m) {
     }
     return (int)hash;
 }
+
+// hash_table.c
+static int ht_get_hash(
+        const char* s, const int num_buckets, const int attempt
+) {
+    const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
+    const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
